ab1751f45faa25fbc18a22758e94a89e7098e3b6
Abid Ali <abdsoftfsd@gmail.com>
2021-01-19 19:57:05 +0500

    creating and implementing mixins in typescript

--- lessons/8-Extending-the -Functionality-of-Classes-with-Mixins/lesson.1.begin.ts
+++ lessons/8-Extending-the -Functionality-of-Classes-with-Mixins/lesson.1.begin.ts
@@ -1,40 +1,53 @@
 import { Project, Size } from "./types";
 
-class ApiClient {
-  baseUrl: string;
-
-  constructor(baseUrl: string) {
-    this.baseUrl = baseUrl;
-  }
-
-  getProjects(): Promise<Project[]> {
-    return Promise.resolve([]);
-  }
-
-  saveProject(): Promise<string> {
-    return Promise.resolve("1");
-  }
-
-  deleteProject(): Promise<void> {
-    return Promise.resolve();
-  }
-
-  getImages(): Promise<{
-    src: string;
-    dimensions: Size;
-  }> {
-    return Promise.resolve({
-      src: "http://a-fake-url.com/image-id",
-      dimensions: {
-        height: 100,
-        width: 300
-      }
-    });
-  }
-
-  saveImage(src: string): Promise<number> {
-    return Promise.resolve(10);
+export class CoreApi {
+  constructor(public baseUrl: string) {
   }
 }
 
+export type BaseApiConstructor = new (...args: any[]) => CoreApi;
+
+function ImagesApi<TBase extends BaseApiConstructor>(Base: TBase) {
+  return class extends Base {
+    getImages(): Promise<{
+      src: string;
+      dimensions: Size;
+    }> {
+      return Promise.resolve({
+        src: "http://a-fake-url.com/image-id",
+        dimensions: {
+          height: 100,
+          width: 300
+        }
+      });
+    }
+  
+    saveImage(src: string): Promise<number> {
+      return Promise.resolve(10);
+    }
+  }
+}
+
+function ProjectsApi<TBase extends BaseApiConstructor>(Base: TBase) {
+  return class extends Base {
+    getProjects(): Promise<Project[]> {
+      return Promise.resolve([]);
+    }
+  
+    saveProject(): Promise<string> {
+      return Promise.resolve("1");
+    }
+  
+    deleteProject(): Promise<void> {
+      return Promise.resolve();
+    }
+  }
+}
+
+class ApiClient extends ProjectsApi(ImagesApi(CoreApi)) {
+
+}
+
 const api = new ApiClient("http://pluralsight.com");
+
+// api.
